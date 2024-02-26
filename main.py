4c1b334239d0400196de1d3a715146f60dd3c457
Sofia Llàcer Caro <sofiallacercaro@gmail.com>
2024-02-26 18:26:32 +0100

    update preprocess

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python
 
 from config import FILE
-from manager import read_document
+from manager import read_file, preprocess
 
 def main():
     file = FILE
 
-    # 1. Read twitter teat data from file tweets.txt
-    text = read_document(file)
-    #result = print(len(text), "lines, ", )
-    result = len(text.splitlines())
-
+    # 1. Read twitter tweat data from file tweets.txt
+    tweets = read_file(file)
+    
     # 2. Create corpus of document by using this data
-    #corpus = into_corpus(text)
+    corpus = [] # Initialize corpus
+    for tweet in tweets:
+        corpus.append(tweet.strip())
+
     # 3. Preprocess data for required clearning up to gain insight about data
-    # 4. Represent document di in matrix form
+    for document in range(len(corpus)):
+        a = preprocess(corpus[document])
+        print(a)
+    #result = preprocess(corpus[5])
+
+
+    # 4. Represent document in matrix form
     # 5. Find most relevant words from all documents
     # 6. Visualize word frequencies found in document term matrix using wordcloud
     # 7. What can we tell abotu tweets by looking at wordcloud?
     # 8. Find the most similar tweets from twitter data
 
-    return result
+    return 'result'
 
 if __name__ == "__main__":
     print(main())
--- manager.py
+++ manager.py
@@ -9,74 +9,65 @@ import sys
 from config import FILE, STOP_WORDS, BAG_OF_WORDS
 
 
-def read_document(document:str) -> str: 
+def read_file(file:str) -> list: 
     '''
     Takes in the location of a .txt file, returns the contents of a .txt file into a string.
-    :input: document; .txt file to be read
-    :output: data; contents of the .txt file
+    Structures the contents into documents and a corpus
+    :input: file; .txt file to be read
+    :output: documents; list where each of the elements are the documents as extracted from the file
     '''
     try:
-        with open(document, 'r') as f:
-            data = f.read()
-        return data
+        with open(file, 'r', encoding="utf8") as f:
+            documents = f.readlines()
+        return documents
       
     except IOError:
-        print("Error opening or reading input file: ", document)
+        print("Error opening or reading input file: ", file)
         sys.exit()
 
 
-def reformatting(text:str) -> list:
-    '''
-    Reformats a text into documents and a corpus for later text mining use
-    :input: text; full text in string type
-    :output: document; each of the documents or "sentences" that the text has been divided onto
-    :output: corpus; ensemble of documents in the corpus list object
-    '''
-    document = []
-    corpus = []
-    lines = text.splitlines() #  Here we consider different lines to be each of the documents, as we're dealing with tweets
-    for line in lines:
-        document.append(line)
-    return corpus
 
-
-def preprocessing() -> str:
+def preprocess(text:str) -> str:
     '''
     Preprocesses a .txt file and applies case normalization for later use in text mining tasks
+    :input: text; string containing text to be preprocessed
+    :output: doc; string of preprocessed text
     '''
-    file = FILE
-    raw_content = open(file, 'r', encoding="utf8").read()     # Added encoding to ensure spanish characters identified
 
-    stop_words = STOP_WORDS
+    #stop_words = STOP_WORDS
 
     # Make all text lowercase
-    t1 = raw_content.lower() # lowercase
+    doc = text.lower() # lowercase
     
     # Remove punctuation.Used custom punctuation based on unicode instead of using method string.punctuation.
     # This is because the latter is ASCII, which doesn't include spanish punctuation sign ¡
     punctuation = "".join((chr(i) for i in range(sys.maxunicode) if unicodedata.category(chr(i)).startswith('P')))
-    t2 = t1.translate(str.maketrans('', '', punctuation))  
+    doc = doc.translate(str.maketrans('', '', punctuation))  
     
     # Remove numbers
-    t3 = re.sub(r'\d+', '', t2)
+    doc = re.sub(r'\d+', '', doc)
 
     # Remove white space
-    t4 = t3.strip()
+    doc = doc.strip()
 
     # Remove mentions
-    t5 = re.sub("@\S+", "", t4)
+    doc = re.sub(r"@\S+", "", doc)
+    doc = re.sub(r'#\w+', '', doc)
 
     # Remove links
-    t6 = re.sub("https?:\/\/.*[\r\n]*", "", t5)
+    doc = re.sub(r'http\S+', '', doc)
+    
+    # Remove b at the beginning of tweets
+    doc = re.sub(r'^b', '', doc)
 
     # Remove hashtags
-    t7 = re.sub("#", "", t6)
+    doc = re.sub("#", "", doc)
 
     # Remove tickers
-    t8 = re.sub("\$", "", t7)
+    doc = re.sub("\$", "", doc)
 
     # Remove stopwords
-    clean_text = " ".join([word for word in t8.split() if word not in stop_words])
+    #clean_text = " ".join([word for word in t8.split() if word not in stop_words])
 
-    return clean_text
+    return doc
 
